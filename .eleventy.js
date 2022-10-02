968eeeb04a5fefb800256ca3d3e379ab49923d90
Nanako <00sh.chiha+github@gmail.com>
2022-10-02 18:43:39 +0900

    use default out

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
     return {
         dir: {
-            input: "src",
-            output: "docs"
+            input: "src"
         }
     }
 }
