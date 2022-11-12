8e86f1bc94890c8496b6c7ed85044b11630cb295
Nanako <00sh.chiha+github@gmail.com>
2022-11-12 22:15:24 +0900

    Moving to Astro - Change Design
    Co-Authored-By: Lance Ross <102563271+lancerossdev@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import image from "@astrojs/image";
 
 
 export default defineConfig({
-  outDir: "./build",
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), image(), compress(
+    {
+      css: true,
+      js: true,
+      html: true,
+      img: true,
+      svg: true,
+    }
+  )],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'body': "sans-serif",
+				'heading': "Inter, sans-serif",
+			},
+			colors: {
+				'accent-light': '#d926a9',
+				'accent-dark': '#1fb2a6',
+
+				'primary-light': '#fff',
+				'primary-dark': '#1a1a1a',
+
+				'secondary-light': '#202020',
+				'secondary-dark': '#202020',
+
+				'tertiary-light': '#e5e7eb',
+				'tertiary-dark': '#202020',
+			}
+		},
 	},
+	darkMode: 'class',
 	plugins: [],
 }
